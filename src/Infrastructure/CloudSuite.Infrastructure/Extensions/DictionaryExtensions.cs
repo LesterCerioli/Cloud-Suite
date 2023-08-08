39bf5b89de357281cdda1ca0f72acf820ccaa925
Ludson Pereira <ludson_ps27@hotmail.com>
2023-08-08 11:27:53 -0300

    issue(#946): Build DictionaryExtensions

--- src/Infrastructure/CloudSuite.Infrastructure/Extensions/DictionaryExtensions.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Extensions/DictionaryExtensions.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Infrastructure.Extensions
 {
-    public class DictionaryExtensions
+    public static class DictionaryExtensions
     {
+        internal static bool TryGetValue<T>(this IDictionary<string, object> dictionary, string key, out T value)
+        {
+            if (dictionary.TryGetValue(key, out var valueObj) && valueObj is T variable)
+            {
+                value = variable;
+                return true;
+            }
+
+            value = default(T);
+            return false;
+        }
+
+
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.TryGetValue(key, out var obj) ? obj : default(TValue);
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> factory)
+        {
+            if (dictionary.TryGetValue(key, out var obj))
+            {
+                return obj;
+            }
+
+            return dictionary[key] = factory(key);
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> factory)
+        {
+            return dictionary.GetOrAdd(key, k => factory());
+        }
     }
 }
