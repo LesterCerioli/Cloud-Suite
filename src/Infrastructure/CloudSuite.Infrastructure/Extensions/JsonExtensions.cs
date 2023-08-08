e4ca40acb627fd4d134d024756c6943d803439c7
Ludson Pereira <ludson_ps27@hotmail.com>
2023-08-08 12:09:26 -0300

    issue(#946): Build Extensions classes into Infrastrcuture Project

--- src/Infrastructure/CloudSuite.Infrastructure/Extensions/JsonExtensions.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Extensions/JsonExtensions.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CloudSuite.Infrastructure.Extensions
 {
-    internal class JsonExtensions
+    public static class JsonExtensions
     {
+        public static string ToJsonString(this object obj, bool camelCase = false, bool indented = false)
+        {
+            var options = new JsonSerializerSettings();
+
+            if (camelCase)
+            {
+                options.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            }
+
+            if (indented)
+            {
+                options.Formatting = Formatting.Indented;
+            }
+
+            return JsonConvert.SerializeObject(obj, options);
+        }
     }
 }
