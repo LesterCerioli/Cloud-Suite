66a021c69b0b80c951af3b3066172e8baa20bec2
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-08-09 15:33:52 -0400

    "issue(#957): Resolving bug"

--- src/Modules/CloudSuite.Modules.Application/Core/FailFastRequestBehavior.cs
+++ src/Modules/CloudSuite.Modules.Application/Core/FailFastRequestBehavior.cs
@@ -31,7 +31,7 @@ namespace CloudSuite.Modules.Application.Core
             return Task.FromResult(response as TResponse);
         }
 
-        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             var failures = _validators
                 .Select(v => v.Validate((IValidationContext)request))
