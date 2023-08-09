beab9b8b1d50a4dcf5b41a16890788c9226509c7
Nelson Villarreal <nelson.villarreal@lucastechnologyservice.com>
2023-08-09 16:05:10 -0400

    "feature/#948:Build Data classes into Infrastruct"

--- src/Infrastructure/CloudSuite.Infrastructure/Data/IRepositoryWithTypedId.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/IRepositoryWithTypedId.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Infrastructure.Data
 {
-    public class IRepositoryWithTypedId
+    public interface IRepositoryWithTypedId<T, TId> where T : IEntityWithTypeId<TId>
     {
+        IQueryable<T> Query();
+
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entity);
+
+        IDbContextTransaction BeginTransaction();
+
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+
+        void Remove(T entity);
     }
 }
