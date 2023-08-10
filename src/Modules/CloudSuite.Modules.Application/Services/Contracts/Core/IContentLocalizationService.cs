6f4c26aab0d5178bd69b25d2cb26ab8a21ad0bc4
Gabriel Ortega <gabriel.ortega@lucastechnologyservice.com>
2023-08-10 11:17:11 -0300

    "issue(#961): Publish branch on azure"

--- src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/IContentLocalizationService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/IContentLocalizationService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CloudSuite.Infrastructure.Models;
 
 namespace CloudSuite.Modules.Application.Services.Contracts.Core
 {
     public interface IContentLocalizationService
     {
+        string GetLocalizedProperty<TEntity>(TEntity entity, string propertyName, string propertyValue) where TEntity : EntityBase;
+
+        string GetLocalizedProperty<TEntity>(TEntity entity, string propertyName, string propertyValue, string cultureId) where TEntity : EntityBase;
     }
 }
