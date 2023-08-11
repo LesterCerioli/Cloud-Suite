f292cf56974c67c0984ead7e34e3e7d608acb127
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-08-10 21:45:18 -0400

    "issue(#954): Building Fiscal Entity models"

---
+++ src/Modules/CloudSuite.Modules.Domain/Models/Fiscal/SealedExampleClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Domain.Models.Fiscal
+{
+    public partial class SealedExampleClass
+    {
+        public PedSitNfse PedSitNfse { get; set; }
+    }
+}
---
+++ src/Modules/CloudSuite.Modules.Domain/ValueObjects/Services.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Domain.ValueObjects
+{
+    public enum Services
+    {
+        #region NFSe
+        /// <summary>
+        /// Enviar Lote RPS NFS-e 
+        /// </summary>
+        RecepcionarLoteRps,
+        /// <summary>
+        /// Consultar Situação do lote RPS NFS-e
+        /// </summary>
+        ConsultarSituacaoLoteRps,
+        /// <summary>
+        /// Consultar NFS-e por RPS
+        /// </summary>
+        ConsultarNfsePorRps,
+        /// <summary>
+        /// Consultar NFS-e por NFS-e
+        /// </summary>
+        ConsultarNfse,
+        /// <summary>
+        /// Consultar lote RPS
+        /// </summary>
+        ConsultarLoteRps,
+        /// <summary>
+        /// Cancelar NFS-e
+        /// </summary>
+        CancelarNfse,
+        /// <summary>
+        /// Consultar a URL de visualização da NFSe
+        /// </summary>
+        ConsultarURLNfse,
+        #endregion
+
+        /// <summary>
+        /// Nulo / Nenhum serviço em execução
+        /// </summary>        
+        Nulo
+    }
+}
