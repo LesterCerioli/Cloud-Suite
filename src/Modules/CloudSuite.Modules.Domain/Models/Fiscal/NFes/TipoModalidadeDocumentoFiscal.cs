4571aeec45423e4b815766d35f76e8c3538162ff
dc100 <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-08-14 20:36:48 -0400

    issue(#954): Building Fisdcal Entity models

---
+++ src/Modules/CloudSuite.Modules.Domain/Models/Fiscal/NFes/TipoModalidadeDocumentoFiscal.cs
@@ -0,0 +1,15 @@
+namespace CloudSuite.Modules.Domain.Models.Fiscal.NFes
+{
+    public enum TipoModalidadeDocumentoFiscal
+    {
+        // <summary>
+        /// Nota Fiscal Eletrônica (NF-e).
+        /// </summary>
+        Nfe = 55,
+
+        /// <summary>
+        /// Nota Fiscal de Consumidor Eletrônica (NFC-e).
+        /// </summary>
+        
+    }
+}
---
+++ src/Modules/CloudSuite.Modules.Domain/Models/Fiscal/NFes/TipoNotaFiscal.cs
@@ -0,0 +1,16 @@
+namespace CloudSuite.Modules.Domain.Models.Fiscal.NFes
+{
+    public enum TipoNotaFiscal
+    {
+        /// <summary>
+        /// Nota Fiscal de Entrada (0).
+        /// </summary>
+        Entrada = 0,
+
+        /// <summary>
+        /// Nota Fiscal de Saída (1).
+        /// </summary>
+        Saida = 1
+        
+    }
+}
