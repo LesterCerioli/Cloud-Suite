0963dfbb60d07ca16b1179c831a8e2c46b15963d
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-08-17 07:53:04 -0300

    "issue(#967): Building Core mapping classes structure"

---
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Mappimgs/EFCore/Core/DistrictMap.cs
@@ -0,0 +1,19 @@
+﻿using CloudSuite.Modules.Domain.Models.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Core
+{
+    public class DistrictMap : IEntityTypeConfiguration<District>
+    {
+        public void Configure(EntityTypeBuilder<District> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Mappimgs/EFCore/Core/UserRoleMap.cs
@@ -0,0 +1,19 @@
+﻿using CloudSuite.Modules.Domain.Models.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Core
+{
+    public class UserRoleMap : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
