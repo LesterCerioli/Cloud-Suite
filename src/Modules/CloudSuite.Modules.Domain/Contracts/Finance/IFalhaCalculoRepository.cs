34eb94ed4b5e1299f41f536511b5408e774ceace
Karolina Gusmao <karolinab2gusmao@gmail.com>
2023-08-18 13:01:40 -0300

    "feature(#982): Build Domain Contracts into Finance Context"

---
+++ src/Modules/CloudSuite.Modules.Domain/Contracts/Finance/IFalhaCalculoRepository.cs
@@ -0,0 +1,22 @@
+﻿using CloudSuite.Modules.Domain.Models.Finance;
+using CloudSuite.Modules.Domain.Models.Fiscal.NFes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Domain.Contracts.Finance
+{
+    public interface IFalhaCalculoRepository
+    {
+        Task<FalhaCalculo> GetByErro(string erro);
+        Task<FalhaCalculo> GetByMensagem(string mensagem);
+
+        Task<IEnumerable<FalhaCalculo>> GetList();
+
+        Task Add(FalhaCalculo falhaCalculo);
+        void Update(FalhaCalculo falhaCalculo);
+        void Remove(FalhaCalculo falhaCalculo);
+    }
+}
