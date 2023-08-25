905d1847a99ef8b9def05e2ae11989870696d271
LesterCerioli <lesterlucasit@hotmail.com>
2023-08-25 14:12:07 -0300

    "issue(#963):  Building Core View Models"

---
+++ src/Modules/CloudSuite.Modules.Application/ViewModels/Core/CompanyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.ViewModels.Core
+{
+    public class CompanyViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [DisplayName("CnpjNumero")]
+        public string? CnpjNumber { get; private set; }
+    }
+}
