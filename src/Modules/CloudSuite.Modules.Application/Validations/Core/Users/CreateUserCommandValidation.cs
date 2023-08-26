17a5127d658be0024d0228071a997369f66b730f
LesterCerioli <lesterlucasit@hotmail.com>
2023-08-26 19:29:11 -0300

    "issue(#960): Building validations"

--- src/Modules/CloudSuite.Modules.Application/Validations/Core/Users/CreateUserCommandValidation.cs
+++ src/Modules/CloudSuite.Modules.Application/Validations/Core/Users/CreateUserCommandValidation.cs
@@ -9,11 +9,11 @@ namespace CloudSuite.Modules.Application.Validations.Core.Users
     {
       RuleFor(command => command.FullName)
       .NotEmpty()
-      .WithMessage("FullName deve ser preenchido")
+      .WithMessage("O NOME deve ser preenchido")
       .MinimumLength(3)
-      .WithMessage("FullName muito curto, deve conter mais de 3 caracteres")
-      .MaximumLength(30)
-      .WithMessage("FullName muito longo, deve conter até 30 caracteres");
+      .WithMessage("NOME muito curto, deve conter mais de 3 caracteres")
+      .MaximumLength(450)
+      .WithMessage("NOME muito longo, deve conter até 450 caracteres");
       
 
       RuleFor(command => command.Email)
@@ -27,35 +27,17 @@ namespace CloudSuite.Modules.Application.Validations.Core.Users
 
       RuleFor(command => command.Vendor)
       .NotEmpty()
-      .WithMessage("Vendor deve ser preenchido");
+      .WithMessage("Fornecedor deve ser preenchido");
 
-      RuleFor(command => command.IsDeleted)
-      .NotNull()
-      .WithMessage("IsDeleted deve ser preenchido");
-
-      RuleFor(command => command.CreatedOn)
-      .NotEmpty().WithMessage("CreatedOn deve ser preenchido")
-      .Must(date => date != default(DateTimeOffset)).WithMessage("CreatedOn deve ser uma data/hora válida");
       
-      RuleFor(command => command.LatestUpdatedOn)
-      .NotEmpty().WithMessage("LatestUpdatedOn deve ser preenchido")
-      .Must(date => date != default(DateTimeOffset)).WithMessage("LatestUpdatedOn deve ser uma data/hora válida");
 
-      RuleFor(command => command.Culture)
-      .NotEmpty()
-      .WithMessage("Culture deve ser preenchido")
-      .MinimumLength(3)
-      .WithMessage("Culture muito curto, deve conter mais de 3 caracteres")
-      .MaximumLength(50)
-      .WithMessage("Culture muito longo, deve conter até 50 caracteres");
+      
+      
+      
 
-      RuleFor(command => command.ExtensionData)
-      .NotEmpty()
-      .WithMessage("ExtensionData deve ser preenchido")
-      .MinimumLength(3)
-      .WithMessage("ExtensionData muito curto, deve conter mais de 3 caracteres")
-      .MaximumLength(50)
-      .WithMessage("ExtensionData muito longo, deve conter até 50 caracteres");
+      
+
+      
     }          
   }
 }
