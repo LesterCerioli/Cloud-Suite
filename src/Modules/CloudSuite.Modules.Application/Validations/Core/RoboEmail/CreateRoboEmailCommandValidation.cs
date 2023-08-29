36d33a15d5ac763079c79a63b64dbef6bd6ad945
LesterCerioli <lesterlucasit@hotmail.com>
2023-08-29 19:24:58 -0300

    "Resolving Validations bugs"

--- src/Modules/CloudSuite.Modules.Application/Validations/Core/RoboEmail/CreateRoboEmailCommandValidation.cs
+++ src/Modules/CloudSuite.Modules.Application/Validations/Core/RoboEmail/CreateRoboEmailCommandValidation.cs
@@ -10,30 +10,27 @@ namespace CloudSuite.Modules.Application.Validations.Core.RoboEmail
       RuleFor(command => command.EmailAddressSender)
       .NotEmpty()
       .WithMessage("Remetente do endereço de e-mail deve ser preenchida")
-      .MinimumLength(3)
-      .WithMessage("Remetente do endereço de e-mail muito curto, deve conter mais de 3 caracteres");
+      .MaximumLength(100)
+      .WithMessage("Remetente do endereço de e-mail deve conter no máximo 100 caracteres");
 
       RuleFor(command => command.Subject)
       .NotEmpty()
       .WithMessage("Assunto deve ser preenchida")
-      .MinimumLength(3)
-      .WithMessage("Assunto muito curto, deve conter mais de 3 caracteres");
+      .MaximumLength(10)
+      .WithMessage("Assunto deve conter no máximo 10 caracteres");
 
       RuleFor(command => command.Body)
       .NotEmpty()
       .WithMessage("Menssagem deve ser preenchida")
       .MinimumLength(3)
-      .WithMessage("Menssagem muito curta, deve conter mais de 3 caracteres");
+      .WithMessage("Menssagem deve conter no máximo 100 caracteres");
 
-      RuleFor(command => command.ReceivedTime)
-      .NotEmpty()
-      .WithMessage("ReceivedTime deve ser preenchida");
-
+      
       RuleFor(command => command.MessageRecipient)
       .NotEmpty()
-      .WithMessage("Destinario deve ser preenchida")
-      .MinimumLength(3)
-      .WithMessage("Destinario muito curto, deve conter mais de 3 caracteres");
+      .WithMessage("Destinatáriorio deve ser preenchida")
+      .MinimumLength(100)
+      .WithMessage("Destinatário deve conter no máximo 100 caracteres");
     }
   }
 }
