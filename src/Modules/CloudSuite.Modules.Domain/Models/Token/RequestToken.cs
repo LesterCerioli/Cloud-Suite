41375d8b9c148e0dc1bc8cc6f269d29d5ab8d8a9
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-04 11:12:38 -0300

    feature(#1026): Build RequestToken Entity

---
+++ src/Modules/CloudSuite.Modules.Domain/Models/Token/RequestToken.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using NetDevPack.Domain;
+
+namespace CloudSuite.Modules.Domain.Models.Token
+{
+  public class RequestToken : Entity, IAggregateRoot
+  {
+    public int Id { get; set; }
+
+    public Guid RequestId { get; set; }
+
+    [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+    [MaxLength(40)]
+    public string FullName { get; set; }
+
+    [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+    [MaxLength(2)]
+    public string PhoneRegion { get; set; }
+
+    [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+    [MaxLength(13)]
+    public string Phone { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public DateTime? Validated { get; set; }
+
+    [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+    [MaxLength(4)]
+    public string Token { get; set; }
+
+    public RequestToken()
+    {
+
+    }
+
+    public RequestToken(Guid requestId, string fullName, string phoneRegion, string phone, DateTime created)
+    {
+      RequestId = requestId;
+      FullName = fullName;
+      PhoneRegion = phoneRegion;
+      Phone = phone;
+      Created = created;
+      Token = GetToken();
+    }
+    private string GetToken()
+    {
+      Random rand = new Random();
+      int on = rand.Next(0, 9);
+      int tw = rand.Next(0, 9);
+      int tr = rand.Next(0, 9);
+      int fo = rand.Next(0, 9);
+
+      return on.ToString() + tw.ToString() + tr.ToString() + fo.ToString();
+    }
+  }
+}
