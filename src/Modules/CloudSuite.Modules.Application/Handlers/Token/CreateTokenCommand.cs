9a7dae333198979b9c2d15e0f33ca251ef4252c3
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-04 13:28:46 -0300

    feature(#1026): Build handle CreateTokenCommand

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Token/CreateTokenCommand.cs
@@ -0,0 +1,37 @@
+using TokenEntity = CloudSuite.Modules.Domain.Models.Token.RequestToken;
+using CloudSuite.Modules.Application.Handlers.Token.Responses;
+using MediatR;
+
+namespace CloudSuite.Modules.Application.Handlers.Token
+{
+  public class CreateTokenCommand : IRequest<CreateTokenResponse>
+  {
+    public CreateTokenCommand()
+    {
+      Id = Guid.NewGuid();
+    }
+
+    public TokenEntity GetEntity()
+    {
+      return new TokenEntity(
+        this.Id,
+        this.FullName,
+        this.PhoneRegion,
+        this.Phone,
+        this.Created
+      );
+    }
+
+    public Guid Id { get; private set; }
+
+    public string FullName { get; set; }
+
+    public string PhoneRegion { get; set; }
+
+    public string Phone { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public DateTime? Validated { get; set; }
+  }
+}
