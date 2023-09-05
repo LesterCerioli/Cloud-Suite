2399bfbf7e8c1ed081e0c1c9cd5fc2075b0e30be
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-05 14:05:16 -0300

    feature(#1026): Build handlers ValidateToken

--- src/Modules/CloudSuite.Modules.Application/Handlers/Token/ValidateTokenHandlers.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Token/ValidateTokenHandlers.cs
@@ -9,66 +9,69 @@ using MediatR;
 
 namespace CloudSuite.Modules.Application.Handlers.Token
 {
-    [AllowAnonymous]
-    public  class ValidateTokenHandler : IRequestHandler<ValidateTokenRequest, ValidateTokenResponse>
+  [AllowAnonymous]
+  public class ValidateTokenHandler : IRequestHandler<ValidateTokenRequest, ValidateTokenResponse>
+  {
+    private IRequestTokenRepository _requestTokenRepository;
+    private readonly ILogger<ValidateTokenHandler> _logger;
+
+    private int maxValidMinutes = 30;
+
+    public ValidateTokenHandler(IRequestTokenRepository requestTokenRepository, ILogger<ValidateTokenHandler> logger)
     {
-        private IRequestTokenRepository _requestTokenRepository;
-        private readonly ILogger<ValidateTokenHandler> _logger;
-
-        private int maxValidMinutes = 30;
-
-        public ValidateTokenHandler(IRequestTokenRepository requestTokenRepository, ILogger<ValidateTokenHandler> logger)
-        {
-            _requestTokenRepository = requestTokenRepository;
-            _logger = logger;
-        }
-
-        public async Task<ValidateTokenResponse> Handle(ValidateTokenRequest request, CancellationToken cancellationToken)
-        {
-            request.Token = request.Token.Trim();
-
-            _logger.LogInformation($"ValidateTokenRequest: {JsonSerializer.Serialize(request)}");
-
-            // Valida Request
-            var validationResult = new ValidateTokenRequestValidation().Validate(request);
-            try
-            {
-                if (validationResult.IsValid)
-                {
-                    // 2 valida na base de dados se Request Id existe
-
-                    var originalRequest = await _requestTokenRepository.GetByRequestId(Guid.Parse(request.RequestId));
-
-                    if (originalRequest == null)
-                    {
-                        return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Token inválido"));
-                    }
-                    // 3 Se existir na base, valida o tempo
-                    bool TempoInValido = originalRequest.Created.AddMinutes(maxMinutosValidos) < DateTime.Now;
-                    // 4 Valida token
-                    bool TokenDiferente = (originalRequest.Token != request.Token);
-
-                    if (TempoInValido || TokenDiferente || originalRequest.Validated.HasValue)
-                    {
-                        return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Token inválido"));
-                    }
-
-                    // 5 Persistir na base que aquele RequestToken foi validado
-                    originalRequest.Validated = DateTime.Now;
-                    await _requestTokenRepository.Update(originalRequest);
-
-                    return await Task.FromResult(new ValidateTokenResponse(request.Id, true));
-
-                }
-                // 6 Retorna Erro Validacao
-                return await Task.FromResult(new ValidateTokenResponse(request.Id, false, validationResult));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical(ex.Message);
-                return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Não foi possivel Processar solicitação."));
-            }
-            
-        }
+      _requestTokenRepository = requestTokenRepository;
+      _logger = logger;
     }
+
+    public async Task<ValidateTokenResponse> Handle(ValidateTokenRequest request, CancellationToken cancellationToken)
+    {
+      request.Token = request.Token.Trim();
+
+      _logger.LogInformation($"ValidateTokenRequest: {JsonSerializer.Serialize(request)}");
+
+      // Valida Request
+      var validationResult = new ValidateTokenRequestValidation().Validate(request);
+
+      try
+      {
+        if (validationResult.IsValid)
+        {
+          // Valida na base de dados se Request Id existe
+
+          var originalRequest = await _requestTokenRepository.GetByRequestId(Guid.Parse(request.RequestId));
+
+          if (originalRequest == null)
+          {
+            return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Token inválido"));
+          }
+
+          // Se existir na base, valida o tempo
+          bool InvalidTime = originalRequest.Created.AddMinutes(maxValidMinutes) < DateTime.Now;
+
+          // Valida token
+          bool invalidToken = (originalRequest.Token != request.Token);
+
+          if (InvalidTime || invalidToken || originalRequest.Validated.HasValue)
+          {
+            return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Token inválido"));
+          }
+
+          // Persistir na base que aquele RequestToken foi validado
+          originalRequest.Validated = DateTime.Now;
+
+          await _requestTokenRepository.Update(originalRequest);
+
+          return await Task.FromResult(new ValidateTokenResponse(request.Id, true));
+        }
+
+        // Retorna Erro Validacao
+        return await Task.FromResult(new ValidateTokenResponse(request.Id, false, validationResult));
+      }
+      catch (Exception ex)
+      {
+        _logger.LogCritical(ex.Message);
+        return await Task.FromResult(new ValidateTokenResponse(request.Id, false, "Não foi possivel Processar solicitação."));
+      }
+    }
+  }
 }
