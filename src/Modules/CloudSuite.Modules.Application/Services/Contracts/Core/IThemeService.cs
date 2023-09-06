82ce04a2e0a12885dd4b81108cbbe6b48dc0987c
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-06 13:10:22 -0300

    feature(#1080): Correct internal to public

---
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/IThemeService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Services.Contracts.Core
+{
+    public interface IThemeService
+    {
+        
+    }
+}
