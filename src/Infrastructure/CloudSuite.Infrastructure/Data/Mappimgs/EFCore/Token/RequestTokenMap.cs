cb006a2bbb1c332258d50544f07838e57e74fab8
Giovanna <giovannapergentinocs@gmail.com>
2023-09-06 16:17:38 -0300

    "feat(#1028): Build Token Data mappings"

--- src/Infrastructure/CloudSuite.Infrastructure/Data/Mappimgs/EFCore/Token/RequestTokenMap.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Mappimgs/EFCore/Token/RequestTokenMap.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CloudSuite.Modules.Domain.Models.Token;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 
 namespace CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Token   
 {
-    /*
     public class RequestTokenMap : IEntityTypeConfiguration<RequestToken>
     {
         public void Configure(EntityTypeBuilder<RequestToken> builder)
@@ -19,39 +19,41 @@ namespace CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Token
                 .HasColumnType("uniqueidentifier")
                 .IsRequired();
 
-            builder.Property(c => c.NomeCompleto)
-                .HasColumnName("NomeCompleto")
-                .HasColumnType("varchar(40)")
-                .HasMaxLength(40)
-                .IsRequired();
-
-            builder.Property(c => c.TelefoneRegiao)
-                .HasColumnName("TelefoneRegiao")
-                .HasColumnType("varchar(2)")
-                .HasMaxLength(2)
-                .IsRequired();
-
-            builder.Property(c => c.Telefone)
-                .HasColumnName("Telefone")
-                .HasColumnType("varchar(13)")
-                .HasMaxLength(13)
+            builder.Property(c => c.FullName)
+                .HasColumnName("FullName")
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100)
                 .IsRequired();
 
             builder.Property(c => c.Created)
                 .HasColumnName("Created")
-                .HasColumnType("datetime")
+                .HasColumnType("DateTime")
                 .IsRequired();
 
             builder.Property(c => c.Validated)
                 .HasColumnName("Validated")
-                .HasColumnType("datetime");
+                .HasColumnType("DateTime");
 
             builder.Property(c => c.Token)
                 .HasColumnName("Token")
                 .HasColumnType("varchar(4)")
                 .HasMaxLength(4)
                 .IsRequired();
+
+            builder.OwnsOne(p => p.Telephone, telephone =>
+            {
+                telephone.Property(a => a.TelephoneNumber)
+                .HasColumnName("TelephoneNumber")
+                .HasColumnType("varchar(9)")
+                .HasMaxLength(9)
+                .IsRequired(); 
+
+                telephone.Property(a => a.TelephoneRegion)
+                .HasColumnName("TelephoneRegion")
+                .HasColumnType("varchar(2)")
+                .HasMaxLength(2)
+                .IsRequired();
+            });
         }
     }
-    */
 }
