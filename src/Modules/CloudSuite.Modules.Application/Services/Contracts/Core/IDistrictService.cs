9ed5ea6f840d8ee728e35547355c8e554e9d2660
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-06 11:25:30 -0300

    feature(#1080): Delete useless files and build District services contract

---
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/IDistrictService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudSuite.Modules.Application.Handlers.Core.Districts;
+using CloudSuite.Modules.Application.ViewModels.Core;
+
+namespace CloudSuite.Modules.Application.Services.Contracts.Core
+{
+  public interface IDistrictService
+  {
+    Task<DistrictViewModel> GetByName(string name);
+
+    Task Save(CreateDistrictCommand commandCreate);
+  }
+}
