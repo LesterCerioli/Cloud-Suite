6bedc2aac6cfeaf030c67fcca7d9456a07e27615
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-06 11:20:36 -0300

    feature(#1080): Build Country services contract

---
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICountryService.cs
@@ -0,0 +1,12 @@
+﻿using CloudSuite.Modules.Application.Handlers.Core.Countries;
+using CloudSuite.Modules.Application.ViewModels.Core;
+
+namespace CloudSuite.Modules.Application.Services.Contracts.Core
+{
+  internal interface ICountryService
+  {
+    Task<CountryViewModel> GetByName(string countryName);
+
+    Task Save(CreateCountryCommand commandCreate);
+  }
+}
