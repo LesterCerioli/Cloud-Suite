c79040cc6320433bd89f35665d5090d8e3dffe28
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-08 10:04:09 -0300

    issue(#960): Build core handle CreateAppSettingCommand

--- src/Modules/CloudSuite.Modules.Application/Handlers/Core/AppSettings/CreateAppSettingCommand.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Core/AppSettings/CreateAppSettingCommand.cs
@@ -1,12 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CloudSuite.Modules.Application.Handlers.Core.AppSettings.Responses;
+using AppSettingEntity = CloudSuite.Modules.Domain.Models.Core.Address;
+using MediatR;
 
 namespace CloudSuite.Modules.Application.Handlers.Core.AppSettings
 {
-  public class CreateAppSettingCommand
+  public class CreateAppSettingCommand : IRequest<CreateAppSettingResponse>
   {
+    public CreateAppSettingCommand()
+    {
+      Id = Guid.NewGuid();
+    }
 
+    public AppSettingEntity GetEntity()
+    {
+      return new AppSettingEntity(
+        this.Id
+      );
+    }
+
+    public Guid Id { get; private set; }
+
+    public string? Value { get; private set; }
+
+    public string? Module { get; private set; }
+
+    public bool? IsVisibleInCommonSettingPage { get; private set; }
   }
 }
