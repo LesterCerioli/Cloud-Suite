5ee14bc1a08fd511f1ad4f3329ae22f802b0c053
Roberto Valentim <robertosvalentim@hotmail.com>
2023-09-08 14:51:21 -0300

    feature(#1029): Build Token Db Sets

--- src/Infrastructure/CloudSuite.Infrastructure/Data/Core/Context/CloudSuiteDbContext.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Core/Context/CloudSuiteDbContext.cs
@@ -1,6 +1,7 @@
 ﻿
 using CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Core;
 using CloudSuite.Modules.Domain.Models.Core;
+using CloudSuite.Modules.Domain.Models.Token;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -56,6 +57,7 @@ namespace CloudSuite.Infrastructure.Data.Core.Context
 
         public DbSet<WidgetZone> WidgetZones { get; set; }
 
+        public DbSet<RequestToken> RequestTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -84,7 +86,7 @@ namespace CloudSuite.Infrastructure.Data.Core.Context
             modelBuilder.ApplyConfiguration(new WidgetMap());
             modelBuilder.ApplyConfiguration(new WidgetInstanceMap());
             modelBuilder.ApplyConfiguration(new WidgetZoneMap());
-
+            //modelBuilder.ApplyConfiguration(new RequestTokenMap());
 
 
             modelBuilder.Entity<Address>(c =>
@@ -175,6 +177,11 @@ namespace CloudSuite.Infrastructure.Data.Core.Context
             modelBuilder.Entity<WidgetZone>(c =>
             {
                 c.ToTable("WidgetZones");
+            });
+
+            modelBuilder.Entity<RequestToken>(c=>
+            {
+               c.ToTable("RequestTokens"); 
             });
 
             base.OnModelCreating(modelBuilder);
