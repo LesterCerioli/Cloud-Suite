34a902c23412d2baa13f77230d49042e3dffd5b9
Lester Cerioli <lesterlucasit@hotmail.com>
2023-09-09 10:06:51 -0400

    Building District Service

--- src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/DistrictService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/DistrictService.cs
@@ -1,7 +1,24 @@
+using CloudSuite.Modules.Application.Handlers.Districts;
+using CloudSuite.Modules.Application.Services.Contracts;
+
 namespace CloudSuite.Modules.Application.Services.Implementations.Core
 {
     public class DistrictService : IDistrictService
     {
+        private readonly IDistrictRepository _districtRepository;
+        private readonly IMapper _mapper;
+        private readonly IMediatorhandler _mediator;
+        
+        public DistrictService(
+            IDistrictRepository districtRepoisitory,
+            IMediatorHandler mediator,
+            IMapper mapper
+        )
+        {
+            _districtRepository = districtRepoisitory;
+            _mapper = mapper;
+            _mediator = mediator;
+        }
         
     }
 }
