b058432927c315d9a219728991b4216066a0f42a
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-11 14:43:31 -0300

    feature(#9601): Build core handler media CaptionRequest

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Core/Medias/Requests/CheckMediaExistsByCaptionRequest.cs
@@ -0,0 +1,18 @@
+using CloudSuite.Modules.Application.Handlers.Core.Medias.Responses;
+using MediatR;
+
+namespace CloudSuite.Modules.Application.Handlers.Core.Medias.Requests
+{
+  public class CheckMediaExistsByCaptionRequest : IRequest<CheckMediaExistsByFileNameResponse>
+  {
+    public Guid Id { get; private set; }
+
+    public string? Caption { get; set; }
+    
+    public CheckMediaExistsByCaptionRequest(string? caption)
+    {
+      Id = Guid.NewGuid();
+      Caption = caption;
+    }
+  }
+}
