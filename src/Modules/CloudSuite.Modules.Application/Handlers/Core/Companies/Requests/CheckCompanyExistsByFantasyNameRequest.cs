9d9939dff7430b79d4431946d98fa6ac2ecc93a9
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-11 11:56:42 -0300

    feature(#9601): Build core handler company FantasyNameRequest

--- src/Modules/CloudSuite.Modules.Application/Handlers/Core/Companies/Requests/CheckCompanyExistsByFantasyNameRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Core/Companies/Requests/CheckCompanyExistsByFantasyNameRequest.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CloudSuite.Modules.Application.Handlers.Core.Companies.Responses;
+using MediatR;
 
 namespace CloudSuite.Modules.Application.Handlers.Core.Companies.Requests
 {
-    public class CheckCompanyExistsByFantasyNameRequest
+  public class CheckCompanyExistsByFantasyNameRequest : IRequest<CheckCompanyExistsByFantasyNameResponse>
+  {
+    public Guid Id { get; private set; }
+
+    public string? FantasyName { get; set; }
+
+    public CheckCompanyExistsByFantasyNameRequest(string fantasyName)
     {
-        
+      Id = Guid.NewGuid();
+      FantasyName = fantasyName;
     }
+  }
 }
