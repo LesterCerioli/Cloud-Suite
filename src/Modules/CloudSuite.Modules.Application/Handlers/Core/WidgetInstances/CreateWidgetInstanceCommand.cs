19a1ac719cbee4da084a4787f8b927d2de1ac411
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-12 11:20:29 -0300

    feature(#9601): Build core handler WidgetInstance CreateCommand

--- src/Modules/CloudSuite.Modules.Application/Handlers/Core/WidgetInstances/CreateWidgetInstanceCommand.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Core/WidgetInstances/CreateWidgetInstanceCommand.cs
@@ -1,7 +1,25 @@
+using WidgetInstanceEntity = CloudSuite.Modules.Domain.Models.Core.WidgetInstance;
+using MediatR;
+
 namespace CloudSuite.Modules.Application.Handlers.Core.WidgetInstances
 {
-  public class CreateWidgetInstanceCommand
+  public class CreateWidgetInstanceCommand : IRequest<CreateWidgetInstanceResponse>
   {
+    public CreateWidgetInstanceCommand()
+    {
+      Id = Guid.NewGuid();
+    }
 
+    public WidgetInstanceEntity GetEntity()
+    {
+      return new WidgetInstanceEntity(
+        this.Name
+      );
+    }
+
+
+    public Guid Id { get; private set; }
+
+    public string? Name { get; set; }
   }
 }
