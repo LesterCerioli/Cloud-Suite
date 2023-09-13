fc43da3f210062f77aba8524c323e8fd42f2fb15
Giovanna <giovannapergentinocs@gmail.com>
2023-09-13 19:21:27 -0300

    "feat(#1117): Build Company Service and Company/Cnpj handlers"

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Core/Companies/Requests/CheckCompanyExistsByCnpjRequest.cs
@@ -0,0 +1,19 @@
+﻿using CloudSuite.Modules.Application.Handlers.Core.Companies.Responses;
+using CloudSuite.Modules.Domain.ValueObjects;
+using MediatR;
+
+namespace CloudSuite.Modules.Application.Handlers.Core.Companies.Requests
+{
+    public class CheckCompanyExistsByCnpjRequest : IRequest<CheckCompanyExistsByCnpjResponse>
+    {
+        public Guid Id { get; private set; }
+
+        public Cnpj cnpj { get; set; }
+
+        public CheckCompanyExistsByCnpjRequest(Cnpj cnpj)
+        {
+            Id = Guid.NewGuid();
+            //Cnpj = cnpj;
+        }
+    }
+}
---
+++ src/Modules/CloudSuite.Modules.Application/Validations/Core/Companies/CheckCompanyExistsByCnpjRequestValidation.cs
@@ -0,0 +1,19 @@
+﻿using CloudSuite.Modules.Application.Handlers.Core.Companies.Requests;
+using FluentValidation;
+
+namespace CloudSuite.Modules.Application.Validations.Core.Companies
+{
+    public class CheckCompanyExistsByCnpjRequestValidation : AbstractValidator<CheckCompanyExistsByCnpjRequest>
+    {
+        public CheckCompanyExistsByCnpjRequestValidation()
+        {
+            /*
+            RuleFor(request => request.Cnpj)
+            .NotEmpty()
+            .WithMessage("Nome fantasia deve ser preenchido")
+            .MaximumLength(100)
+            .WithMessage("Nome fantasia deve possuir no máximo 100 caracteres");
+            */
+        }
+    }
+}
