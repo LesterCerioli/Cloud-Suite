22ff40346346244d4bfced3833783a8480798c98
igums <igormsantos1@icloud.com>
2023-09-13 14:15:05 -0300

    "feature(#1084):  build app setting api controller"

--- src/Rest/CloudSuite.Services.Core.API/Controllers/v1/AppSettingApiController.cs
+++ src/Rest/CloudSuite.Services.Core.API/Controllers/v1/AppSettingApiController.cs
@@ -1,43 +1,127 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using CloudSuite.Modules.Application.Handlers.Core.AppSettings;
+using CloudSuite.Modules.Application.Handlers.Core.AppSettings.Requests;
+using CloudSuite.Modules.Domain.Contracts.Core;
+using CloudSuite.Modules.Domain.Models.Core;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CloudSuite.Services.Core.API.Controllers.v1
 {
+
     [Route("api/[controller]")]
     [ApiController]
+
     public class AppSettingApiController : ControllerBase
     {
-        // GET: api/<AppSettingApiController>
+        private readonly IMediator _mediator;
+        private readonly IAppSettingRepository _appSettingRepository;
+        public AppSettingApiController(IMediator mediator, IAppSettingRepository appSettingRepository)
+        {
+            _mediator = mediator;
+            _appSettingRepository = appSettingRepository;
+        }
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("{value}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetByAppSetting([FromRoute] string value)
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var result = await _mediator.Send(new CheckAppSettingExistsByAppSettingRequest(value));
+                if (result.Errors.Any())
+                {
+                    return BadRequest(result);
+                }
+                if (result.Exists)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching App setting by App Setting: {ex.Message}.");
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An internal Server Error" });
+            }
         }
 
-        // GET api/<AppSettingApiController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+
+        [HttpGet]
+        [Route("{module}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetByModule([FromRoute] string module)
         {
-            return "value";
+            try
+            {
+                var result = await _mediator.Send(new CheckAppSettingExistsByModuleRequest(module));
+                if (result.Errors.Any())
+                {
+                    return BadRequest(result);
+                }
+                if (result.Exists)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching app setting by module: {ex.Message}.");
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An internal Server Error" });
+            }
         }
 
-        // POST api/<AppSettingApiController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IEnumerable<AppSetting>> GetAll()
+        {
+            var appsetting = await _appSettingRepository.GetAll();
+            
+            return appsetting;
+        }
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add([FromBody] CreateAppSettingCommand command)
         {
+            try
+            {
+                
+                    var result = await _mediator.Send(command);
+
+                    if (result.Errors.Any())
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest(new { message = "Could not create app setting." });
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An internal Server Error" });
+            }
         }
 
-        // PUT api/<AppSettingApiController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AppSettingApiController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
-}
+   }
