fd591b5ac7c896a12b0f4281c6378c792b4fb0d6
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-14 13:57:23 -0300

    feature(#10802): Build Contract Services Company

--- src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICompanyService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICompanyService.cs
@@ -1,9 +1,12 @@
+using CloudSuite.Modules.Application.Handlers.Core.Companies;
 using CloudSuite.Modules.Application.ViewModels.Core;
 
 namespace CloudSuite.Modules.Application.Services.Contracts.Core
 {
   public interface ICompanyService
   {
-    Task<CompanyViewModel> 
+    Task<CompanyViewModel> GetByFantasyName(string fantasyName);
+
+    Task Save(CreateCompanyCommand commandCreate);
   }
 }
