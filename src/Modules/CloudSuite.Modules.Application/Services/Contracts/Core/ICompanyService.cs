2f244a24d95a33a8c9a6a602c7796ea3b52a6591
Ludson Pereira <ludson_ps27@hotmail.com>
2023-09-14 12:08:14 -0300

    feature(#10802): Build Contract Services Company and WidgetInstance

---
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICompanyService.cs
@@ -0,0 +1,9 @@
+using CloudSuite.Modules.Application.ViewModels.Core;
+
+namespace CloudSuite.Modules.Application.Services.Contracts.Core
+{
+  public interface ICompanyService
+  {
+    Task<CompanyViewModel> 
+  }
+}
