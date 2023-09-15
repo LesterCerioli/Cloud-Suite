3f1e49fdcccd8719666777f9b1be05278b86dc5f
LesterCerioli <lesterlucasit@hotmail.com>
2023-09-15 10:25:14 -0300

    "feat(#1080): Adjusting Company Service Contract"

--- src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICompanyService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/Core/ICompanyService.cs
@@ -1,12 +1,20 @@
-using CloudSuite.Modules.Application.Handlers.Core.Companies;
+﻿using CloudSuite.Modules.Application.Handlers.Core.Companies;
 using CloudSuite.Modules.Application.ViewModels.Core;
+using CloudSuite.Modules.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CloudSuite.Modules.Application.Services.Contracts.Core
 {
-  public interface ICompanyService
-  {
-    Task<CompanyViewModel> GetByFantasyName(string fantasyName);
+    public interface ICompanyService
+    {
+        Task<CompanyViewModel> GetByFantasyName(string fantasyName);
+        
+        Task<CompanyViewModel> GetByCnpj(Cnpj cnpj);
 
-    Task Save(CreateCompanyCommand commandCreate);
-  }
+        Task Save(CreateCompanyCommand commandCreate);
+    }
 }
