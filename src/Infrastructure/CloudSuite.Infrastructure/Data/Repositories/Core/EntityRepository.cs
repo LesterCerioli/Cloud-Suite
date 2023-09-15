8e966cbbb12ec5e19c7989a1968fca8e04c6effc
igums <igormsantos1@icloud.com>
2023-09-15 19:38:58 -0300

    "feature(#1118): build repositories infrastructure to core context"

---
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Repositories/Core/EntityRepository.cs
@@ -0,0 +1,59 @@
+﻿using CloudSuite.Infrastructure.Data.Core.Context;
+using CloudSuite.Modules.Domain.Contracts.Core;
+using CloudSuite.Modules.Domain.Models.Core;
+using CloudSuite.Modules.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.Data.Repositories.Core
+{
+    public class EntityRepository : IEntityRepository
+    {
+
+        protected readonly CloudSuiteDbContext Db;
+        protected readonly DbSet<Entidade> DbSet;
+
+        public EntityRepository(CloudSuiteDbContext context)
+        {
+            Db = context;
+            DbSet = context.Entidades;
+        }
+
+        public async Task<Entidade> GetBySlug(string slug)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Slug == slug);
+        }
+
+        public async Task<Entidade> GetByName(string name)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.Name == name);
+        }
+
+        public async Task<IEnumerable<Entidade>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task Add(Entidade entity)
+        {
+            await Task.Run(() => {
+                DbSet.Add(entity);
+                Db.SaveChanges();
+            });
+        }
+
+        public void Update(Entidade entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public void Remove(Entidade entity)
+        {
+            DbSet.Remove(entity);
+        }
+    }
+}
