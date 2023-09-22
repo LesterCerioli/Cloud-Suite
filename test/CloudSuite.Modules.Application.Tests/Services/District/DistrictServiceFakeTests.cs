0071911c5b2117a210de031ef97b47fa3a0fcb56
LesterCerioli <lesterlucasit@hotmail.com>
2023-09-22 19:32:43 -0400

    "feat(#1147):Building Services Tests"

---
+++ test/CloudSuite.Modules.Application.Tests/Services/District/DistrictServiceFakeTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using CloudSuite.Modules.Application.AutoMapper;
+using CloudSuite.Modules.Application.Handlers.Core.Districts;
+using CloudSuite.Modules.Application.Services.Contracts.Core;
+using CloudSuite.Modules.Application.Services.Implementations.Core;
+using CloudSuite.Modules.Application.ViewModels.Core;
+using CloudSuite.Modules.Domain.Contracts.Core;
+using CloudSuite.Modules.Domain.Tests.Models;
+using Moq;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Tests.Services.District
+{
+    public class DistrictServiceFakeTests 
+    {
+
+
+        [Fact]
+        public async Task GetByName_Should_Return_DistrictViewModel()
+        {
+            // Arrange
+            var districtRepositoryMock = new Mock<IDistrictRepository>();
+            //districtRepositoryMock.Setup(repo => repo.GetByName(It.IsAny<string>()))
+                //.ReturnsAsync(new DistrictEntityTests(Guid.NewGuid(), "SampleDistrictName", "SampleType", "SampleLocation"));
+
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainToViewModelMappingProfile>(); // Configure AutoMapper profile here
+            });
+            var mapper = mapperConfig.CreateMapper();
+
+            var mediatorHandlerMock = new Mock<IMediatorHandler>();
+            var districtService = new DistrictService(districtRepositoryMock.Object, mapper, mediatorHandlerMock.Object);
+
+            // Act
+            var result = await districtService.GetByName("SampleDistrictName");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<DistrictViewModel>(result);
+            // Add more assertions as needed
+        }
+
+        [Fact]
+        public async Task Save_Should_Add_DistrictEntity()
+        {
+            // Arrange
+            var districtRepositoryMock = new Mock<IDistrictRepository>();
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainToViewModelMappingProfile>(); // Configure AutoMapper profile here
+            });
+            var mapper = mapperConfig.CreateMapper();
+
+            var mediatorHandlerMock = new Mock<IMediatorHandler>();
+            var districtService = new DistrictService(districtRepositoryMock.Object, mapper, mediatorHandlerMock.Object);
+
+            var createDistrictCommand = new CreateDistrictCommand(); // Provide necessary data for the command
+
+            // Act
+            await districtService.Save(createDistrictCommand);
+
+            // Assert
+            //districtRepositoryMock.Verify(repo => repo.Add(It.IsAny<District>()), Times.Once);
+        }
+
+        // Add more test methods for other scenarios and edge cases
+    }
+}
---
+++ test/CloudSuite.Modules.Domain.Tests/Models/DistrictEntityTests.cs
@@ -0,0 +1,142 @@
+﻿using CloudSuite.Modules.Domain.Models.Core;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Domain.Tests.Models
+{
+    public class DistrictEntityTests
+    {
+        [Fact]
+        public void District_Constructor_Sets_Properties_Correctly()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            string name = "SampleDistrict";
+            string type = "SampleType";
+            string location = "SampleLocation";
+
+            // Act
+            var district = new District(id, name, type, location);
+
+            // Assert
+            Assert.Equal(id, district.Id);
+            Assert.Equal(name, district.Name);
+            Assert.Equal(type, district.Type);
+            Assert.Equal(location, district.Location);
+        }
+
+        [Fact]
+        public void District_Default_Constructor_Creates_Instance()
+        {
+            // Act
+            var district = new District();
+
+            // Assert
+            Assert.NotNull(district);
+        }
+
+        [Fact]
+        public void District_States_Collection_Is_Not_Null()
+        {
+            // Arrange
+            var district = new District();
+
+            // Act
+            var states = district.States;
+
+            // Assert
+            Assert.NotNull(states);
+            Assert.IsAssignableFrom<IReadOnlyCollection<State>>(states);
+        }
+
+        [Fact]
+        public void District_StateId_Property_Can_Be_Set()
+        {
+            // Arrange
+            var district = new District();
+            Guid stateId = Guid.NewGuid();
+
+            // Act
+            //district.StateId = stateId;
+
+            // Assert
+            Assert.Equal(stateId, district.StateId);
+        }
+
+        [Fact]
+        public void District_Name_Property_Validation()
+        {
+            // Arrange
+            var district = new District();
+
+            // Act
+            //Name = "SampleName";
+
+            // Assert
+            Assert.Equal("SampleName", district.Name);
+        }
+
+        [Fact]
+        public void District_Type_Property_Validation()
+        {
+            // Arrange
+            var district = new District();
+
+            // Act
+            //district.Type = "SampleType";
+
+            // Assert
+            Assert.Equal("SampleType", district.Type);
+        }
+
+        [Fact]
+        public void District_Location_Property_Validation()
+        {
+            // Arrange
+            var district = new District();
+
+            // Act
+            //district.Location = "SampleLocation";
+
+            // Assert
+            Assert.Equal("SampleLocation", district.Location);
+        }
+
+        [Fact]
+        public void District_Name_Required_Validation()
+        {
+            // Arrange
+            var district = new District();
+
+            // Act & Assert
+            //Assert.Throws<ValidationException>(() => district.Name = null);
+            //Assert.Throws<ValidationException>(() => district.Name = "");
+        }
+
+        //[Fact]
+        //public void District_Type_Required_Validation()
+        //{
+            // Arrange
+            //var district = new District();
+
+            // Act & Assert
+            //Assert.Throws<ValidationException>(() => district.Type = null);
+            //Assert.Throws<ValidationException>(() => district.Type = "");
+        //}
+
+        //[Fact]
+        //public void District_Location_Required_Validation()
+        //{
+            // Arrange
+            //var district = new District();
+
+            // Act & Assert
+            //Assert.Throws<ValidationException>(() => district.Location = null);
+            //Assert.Throws<ValidationException>(() => district.Location = "");
+        //}
+    }
+}
