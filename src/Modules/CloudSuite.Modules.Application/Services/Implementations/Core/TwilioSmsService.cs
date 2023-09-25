10432bfd763582f27a1e4f18b44d3e64f632fa97
LesterCerioli <lesterlucasit@hotmail.com>
2023-09-24 22:28:59 -0400

    "Adjusting branch status"

--- src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/TwilioSmsService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/TwilioSmsService.cs
@@ -6,44 +6,9 @@ using System.Threading.Tasks;
 using CloudSuite.Modules.Application.Services.Contracts.Token;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Twilio;
-using Twilio.Rest.Api.V2010.Account;
-using Twilio.Types;
+
 
 namespace CloudSuite.Modules.Application.Services.Implementations.Core
 {
-    internal class TwilioSmsService : ITwilioSmsServices
-    {
-        private readonly ILogger<ITwilioSmsServices> _logger;
-        private readonly IConfiguration _configuration;
-
-        public TwilioSmsService(ILogger<ITwilioSmsServices> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-
-        public Task SendSMS(string telefone, string msg)
-        {
-            string accountSid = _configuration[""]; //parametros que será atribuido no appSetting
-            string authToken = _configuration[""]; //quando for adquirido os token no twilio
-            string serviceId = _configuration[""];
-
-            try
-            {
-                TwilioClient.Init(accountSid, authToken);
-                var messageOptions = new CreateMessageOptions(new PhoneNumber(telefone));
-                messageOptions.MessagingServiceSid = serviceId;
-                messageOptions.Body = msg;
-                var message = MessageResource.Create(messageOptions);
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError("Unable to send SMS", ex);
-                throw;
-            }
-
-            return Task.CompletedTask;
-        }
-    }
+    
 }
