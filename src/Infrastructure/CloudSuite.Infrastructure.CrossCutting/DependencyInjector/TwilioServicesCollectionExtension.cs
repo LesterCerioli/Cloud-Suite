1e7186b16e6f3ac755ff8600f4e2e4762c16ae8d
igums <igormsantos1@icloud.com>
2023-09-26 10:05:19 -0300

    "feat(#11382): creating TwilioServicesCollectionExtension"

---
+++ src/Infrastructure/CloudSuite.Infrastructure.CrossCutting/DependencyInjector/TwilioServicesCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using CloudSuite.Modules.Application.Services.Contracts.Core;
+using CloudSuite.Modules.Application.Services.Implementations.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.CrossCutting.DependencyInjector
+{
+    public static class TwilioServsicesCollectionExtension
+    {
+        public static IServiceCollection AddTwilioServices(this IServiceCollection services) 
+        {
+            services.AddScoped<ITwilioSmsServices, TwilioSmsServices>();
+
+            return services;
+        }
+    }
+}
