dbc23c42b375263c0a603373f3ec633ff443387c
igums <igormsantos1@icloud.com>
2023-09-26 10:12:15 -0300

    "feat(#1138): building the twilio service in the right place"

--- src/Modules/CloudSuite.Modules.Application/Services/Contracts/Token/ITwilioSmsServices.cs
+++
@@ -1,7 +0,0 @@
-namespace CloudSuite.Modules.Application.Services.Contracts.Token
-{
-  public interface ITwilioSmsServices
-  {
-    Task SendSMS(string telefone, string msg);
-  }
-}
--- src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/TwilioSmsService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Implementations/Core/TwilioSmsService.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CloudSuite.Modules.Application.Services.Contracts.Token;
+﻿using CloudSuite.Modules.Application.Services.Contracts.Core;
+using CloudSuite.Modules.Domain.ValueObjects;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace CloudSuite.Modules.Application.Services.Implementations.Core
 {
-    public class TwilioSmsService
-    
+    public class TwilioSmsServices : ITwilioSmsServices
+    {
+        private readonly ILogger<TwilioSmsServices> _logger;
+        private readonly IConfiguration _configuration;
+        public TwilioSmsServices(ILogger<TwilioSmsServices> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+        public Task SendSMS(string telefone, string msg)
+        {
+            string accountSid = _configuration["Twilio:AccountSid"];
+            string authToken = _configuration["Twilio:AuthToken"];
+            //Dados phone de envio DDI + DDD + PHONE
+            string serviceId = _configuration["Twilio:ServiceID"];
+            //Dados Destino DDI + DDD + PHONE
+            try
+            {
+                TwilioClient.Init(accountSid, authToken);
+                var messageOptions = new CreateMessageOptions(new PhoneNumber(telefone));
+                messageOptions.MessagingServiceSid = serviceId;
+                messageOptions.Body = msg;
+                var message = MessageResource.Create(messageOptions);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Não foi possivel enviar sms via Twilio: ", ex);
+                throw;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
 }
