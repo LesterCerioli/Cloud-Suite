359c3f8037593164be2c874ed05acc119821e91b
LesterCerioli <lesterlucasit@hotmail.com>
2023-10-04 16:12:53 -0300

    "feat(#87): Bui8lding Ms Tests structure"

---
+++ test/MsTests/CloudSuite.Infrastructure.MsTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CloudSuite.Infrastructure.MsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ test/MsTests/CloudSuite.Modules.Application.MsTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CloudSuite.Modules.Application.MsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
