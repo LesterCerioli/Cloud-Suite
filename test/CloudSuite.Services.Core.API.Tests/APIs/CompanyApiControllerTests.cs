ac6dc264be4155a0d7f854e3498b8b5b40a332f6
Giovanna <giovannapergentinocs@gmail.com>
2023-10-05 14:50:00 -0300

    "feat(#89): Create classes test API"

---
+++ test/CloudSuite.Services.Core.API.Tests/APIs/CompanyApiControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Services.Core.API.Tests.APIs
+{
+    internal class CompanyApiControllerTests
+    {
+    }
+}
