8a18a37410cf1d0b6560de6482f78108c5f45623
lester <lesterlucasit@hotmail.com>
2023-10-10 12:59:45 -0400

    feat(#98): Building Address Entity model

---
+++ src/Modules/CloudSuite.Modules.Core/Domain/Models/Address.cs
@@ -0,0 +1,15 @@
+using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Core.Models
+{
+    public class Address : Entity, IAggregateRoot
+    {
+
+    }
+}
