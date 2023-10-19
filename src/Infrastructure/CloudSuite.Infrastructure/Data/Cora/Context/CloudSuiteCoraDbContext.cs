f35b4ad778b43e645b9ac1d61fce76d7446826c5
igor moreira <igormorantosdev@outlook.com>
2023-10-19 14:47:06 -0300

    feat(#1193):  building table extracts on cloudsuitedbcontext"

--- src/Infrastructure/CloudSuite.Infrastructure/Data/Cora/Context/CloudSuiteCoraDbContext.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Cora/Context/CloudSuiteCoraDbContext.cs
@@ -1,7 +1,17 @@
+using CloudSuite.Modules.Domain.Models.Cora.ExtractContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 namespace CloudSuite.Infrastructure.Data.Cora.Context
 {
-    public class CloudSuiteCoraDbContext
+    public class CloudSuiteCoraDbContext : DbContext
     {
-        
+        public CloudSuiteCoraDbContext(DbContextOptions options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Extract> Extracts { get; set; }
     }
 }
