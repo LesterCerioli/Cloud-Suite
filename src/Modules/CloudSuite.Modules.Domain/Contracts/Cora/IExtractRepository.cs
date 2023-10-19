5c1b1b696435e6b1c8087d41d659b79d03125f4b
igor moreira <igormorantosdev@outlook.com>
2023-10-19 13:33:35 -0300

    "feat(#1193):building extract contract"

--- src/Modules/CloudSuite.Modules.Domain/Contracts/Cora/IExtractRepository.cs
+++ src/Modules/CloudSuite.Modules.Domain/Contracts/Cora/IExtractRepository.cs
@@ -1,4 +1,5 @@
-﻿using CloudSuite.Modules.Domain.Models.Cora.ExtractContext;
+﻿using CloudSuite.Modules.Common.Enums.Cora;
+using CloudSuite.Modules.Domain.Models.Cora.ExtractContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,30 @@ namespace CloudSuite.Modules.Domain.Contracts.Cora
         Task<Extract> GetByStartDate(DateTimeOffset dataInicio);
 
         Task<Extract> GetByEndDate(DateTimeOffset dataFinal);
+
+        Task<Extract> GetByType(EntryType entryType);
+
+        Task<Extract> GetByTransactionType(TransactionType transactionType);
+
+        Task<Extract> GetByEntryAmount(decimal amount);
+
+        Task<Extract> GetByEntryCreatedAt(string EntryCreatedAt);
+
+        Task<Extract> GetByEntryTransactionId(string EntryTransactionId);
+
+        Task<Extract> GetByEntryTransactionDescription(string EntryTransactionId);
+
+        Task<Extract> GetByEntryTransactionCounterPartyName(string EntryTransactionCounterPartyName);
+
+        Task<Extract> GetByEntryTransactionCounterPartyIdentity(string EntryTransactionCounterPartyIdentity);
+
+        Task<Extract> GetByAggregationsCreditTotaly(string EntryTransactionCounterPartyIdentity);
+
+        Task<Extract> GetByAggregationsDebitTotal(string EntryTransactionCounterPartyIdentity);
+
+        Task<Extract> GetByHeaderBusinessName(string EntryTransactionCounterPartyIdentity);
+
+        Task<Extract> GetByHeaderBusinessDocument(string EntryTransactionCounterPartyIdentity);
 
         Task<IEnumerable<Extract>> GetList();
 
