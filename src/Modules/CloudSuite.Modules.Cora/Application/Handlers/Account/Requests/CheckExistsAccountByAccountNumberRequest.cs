e57cf75197cb6ac5b2776da3ce13c7591c820260
Lester Cerioli <lesterlucasit@hotmail.com>
2023-10-24 20:34:23 -0300

    feat(#1219): Building Check Accounts Exists by Account number

---
+++ src/Modules/CloudSuite.Modules.Cora/Application/Handlers/Account/Requests/CheckExistsAccountByAccountNumberRequest.cs
@@ -0,0 +1,40 @@
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Account.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Application.Handlers.Account.Requests
+{
+	public class CheckExixtxAccountByAccountNumberRequest : IRequest<CheckExixtxAccountByAccountNumberResponse>
+	{
+        public Guid Id { get; private set; }
+
+		public string? Agency { get; set; }
+
+		public string? AccountNumber { get; set; }
+
+		public string? AccountDigit { get; set; }
+
+		public string? BankName { get; set; }
+
+		public string? BankCode { get; set; }
+
+		public CheckExixtxAccountByAccountNumberRequest(
+			string? agency,
+			string? bankCode,
+			string? accountNumber,
+			string? accountDigit,
+			string? bankName)
+		{
+			Id = Guid.NewGuid();
+			Agency = agency;
+			BankCode = bankCode;
+			AccountNumber = accountNumber;
+			AccountDigit = accountDigit;
+			BankName = bankName;
+		}
+	}
+}
---
+++ src/Modules/CloudSuite.Modules.Cora/Application/Handlers/Account/Responses/CheckExistsAccountByAccountNumberResponse.cs
@@ -0,0 +1,34 @@
+﻿using CloudSuite.Modules.Cora.Application.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Application.Handlers.Account.Responses
+{
+	public class CheckExixtxAccountByAccountNumberResponse : Response
+	{
+		public Guid RequestId { get; private set; }
+
+		public bool Exists { get; set; }
+
+		public CheckExixtxAccountByAccountNumberResponse(Guid requestId, bool exists)
+		{
+			RequestId = requestId;
+			Exists = exists;
+			foreach (var item in Result.Errors)
+			{
+				this.AddError(item.ErrorMessage);
+			}
+		}
+
+		public CheckExixtxAccountByAccountNumberResponse(Guid requestId, string falseValidation) 
+		{
+			RequestId = requestId;
+			Exists = false;
+			this.AddError(falseValidation);
+
+		}
+	}
+}
