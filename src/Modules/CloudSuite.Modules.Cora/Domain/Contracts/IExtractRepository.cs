af20e15df2bbb560a9bdf26cc25a116a69ec15c2
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-10-24 14:37:48 -0300

    "feat(1193): building extract entity and extract repository contract"

---
+++ src/Modules/CloudSuite.Modules.Cora/Domain/Contracts/IExtractRepository.cs
@@ -0,0 +1,52 @@
+﻿using CloudSuite.Modules.Cora.Domain.Models.CloudSuite.Modules.Cora.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Domain.Contracts
+{
+    internal class IExtractRepository
+    {
+        public interface IExtratoRepository
+        {
+            Task<Extract> GetByStartDate(DateTimeOffset dataInicio);
+
+            Task<Extract> GetByEndDate(DateTimeOffset dataFinal);
+
+            //Task<Extract> GetByType(EntryType entryType); nesses dois casos o (EntryType, TransactionType)são ENUMs estão presentes na minha branch mas não na develops.
+
+            //Task<Extract> GetByTransactionType(TransactionType transactionType);
+
+            Task<Extract> GetByEntryAmount(decimal amount);
+
+            Task<Extract> GetByEntryCreatedAt(string EntryCreatedAt);
+
+            Task<Extract> GetByEntryTransactionId(string EntryTransactionId);
+
+            Task<Extract> GetByEntryTransactionDescription(string EntryTransactionId);
+
+            Task<Extract> GetByEntryTransactionCounterPartyName(string EntryTransactionCounterPartyName);
+
+            Task<Extract> GetByEntryTransactionCounterPartyIdentity(string EntryTransactionCounterPartyIdentity);
+
+            Task<Extract> GetByAggregationsCreditTotaly(string EntryTransactionCounterPartyIdentity);
+
+            Task<Extract> GetByAggregationsDebitTotal(string EntryTransactionCounterPartyIdentity);
+
+            Task<Extract> GetByHeaderBusinessName(string EntryTransactionCounterPartyIdentity);
+
+            Task<Extract> GetByHeaderBusinessDocument(string EntryTransactionCounterPartyIdentity);
+
+            Task<IEnumerable<Extract>> GetList();
+
+            Task Add(Extract extrato);
+
+            void Update(Extract extrato);
+
+            void Remove(Extract extrato);
+        }
+    }
+}
