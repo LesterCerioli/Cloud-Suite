97d3228de4a6859f989fca23217f65f0998ac355
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-10-24 16:38:09 -0300

    "feat(#1193) updating extract entity"

--- src/Modules/CloudSuite.Modules.Cora/Domain/Models/Extract.cs
+++ src/Modules/CloudSuite.Modules.Cora/Domain/Models/Extract.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CloudSuite.Modules.Cora.Domain.Models
-{
-    using NetDevPack.Domain;
+﻿    using NetDevPack.Domain;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -88,5 +80,3 @@ namespace CloudSuite.Modules.Cora.Domain.Models
             public string? HeaderBusinessDocument { get; private set; }
         }
     }
-
-}
