00cdd26115fc6990347c2e755b8864826e20b020
Lester Cerioli <lesterlucasit@hotmail.com>
2023-10-25 11:32:28 -0300

    feat(#1226): Building Data repositorues ti Account and Extract entities

--- src/Infrastructure/CloudSuite.Infrastructure/Data/Repositories/Cora/ExtractRepository.cs
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Repositories/Cora/ExtractRepository.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace CloudSuite.Infrastructure.Data.Repositories.Cora
 {
-	public class AccountRepository 
+	public class ExtractRepository 
 	{
 	}
 }
