0c19c71e442f1415a30f56473b6bf9be4c62dc63
Lester Cerioli <lesterlucasit@hotmail.com>
2023-10-25 11:16:25 -0300

    feat(#1219): Building Handlers to Account

---
+++ src/Modules/CloudSuite.Modules.Cora/Application/Handlers/Account/CheckAccountExistsByDigitAccountHandlers.cs
@@ -0,0 +1,24 @@
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Account.Requests;
+using CloudSuite.Modules.Cora.Application.Handlers.Account.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Application.Handlers.Account
+{
+	public class CheckAccountExistsByDigitAccountHandlers : IRequestHandler<CheckAccountExistsByDigitAccountRequest, CheckAccountExistsByDigitAccountResponse>
+	{
+		public CheckAccountExistsByDigitAccountHandlers()
+		{
+
+		}
+
+		public async Task<CheckAccountExistsByDigitAccountResponse> Handle(CheckAccountExistsByDigitAccountRequest request, CancellationToken cancellationToken)
+		{
+			return await Task.FromResult(new CheckAccountExistsByDigitAccountResponse(request.Id, false, validationResult));
+		}
+	}
+}
