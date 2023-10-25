c9dae9391509402ab2d55fc2ea9531a7cbda0c3e
Lester Cerioli <lesterlucasit@hotmail.com>
2023-10-25 00:05:07 -0300

    feat(#1225): Building Extract and Account View models

--- src/Modules/CloudSuite.Modules.Cora/Application/ViewModels/AccountViewModel.cs
+++ src/Modules/CloudSuite.Modules.Cora/Application/ViewModels/AccountViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,25 @@ namespace CloudSuite.Modules.Cora.Application.ViewModels
     {
         [Key]
         public Guid Id { get; set; }
+
+        [DisplayName("Nome Banco")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string BankName { get; set; }
+
+        [DisplayName("Banco Codigo")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string BankCode { get; set; }
+
+        [DisplayName("Agencia")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string Agency { get; set; }
+
+        [DisplayName("Conta Numero")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string AccountNumber { get; set; }
+
+        [DisplayName("Digito Conta")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string DigitAccount { get; set; }
     }
 }
---
+++ src/Modules/CloudSuite.Modules.Cora/Application/ViewModels/ExtractViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Application.ViewModels
+{
+	public class ExtractViewModel
+	{
+		[Key]
+		public Guid Id { get; private set; }
+
+		[DisplayName("Data Inicio")]
+		public DateTimeOffset StartDate { get; set; }
+
+		
+		[DisplayName("Data Fim")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public DateTimeOffset EndDate { get; set; }
+
+		
+		[DisplayName("Valor de Entrada")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public int? EntryAmount { get; set; }
+
+		[DisplayName("Data de Entrada")]
+		[Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+		public string EntryCreatedAt { get; set; }
+
+		[DisplayName("Descricao da Transacao de Entrada")]
+		public string EntryTransactionDescription { get; set; }
+
+
+		[DisplayName("Valor de Credito Total")]
+		public string AggregationsCreditTotal { get; set; }
+
+		[DisplayName("Valor de Debito Total")]
+		public string AggregationsDebitTotal { get; set; }
+				
+		
+	}
+}
