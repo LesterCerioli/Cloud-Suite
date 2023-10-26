31cf6ffdc663a400d9e80016f3c71660a6c18c04
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-10-26 09:13:36 -0300

    "feat(#1193) updating extract repository contracts "

---
+++ src/Modules/CloudSuite.Modules.Cora/Domain/Contracts/IExtractRepository.cs
@@ -0,0 +1,36 @@
+﻿using CloudSuite.Modules.Cora.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Domain.Contracts
+{
+    public interface IExtractRepository
+    {
+        Task<Extract> GetByStartDate(DateTimeOffset dataInicio);
+
+        Task<Extract> GetByEndDate(DateTimeOffset dataFinal);
+
+        Task<Extract> GetByEntryId(string clientName);
+
+        Task<Extract> GetByEntryAmount(decimal amount);
+
+        Task<Extract> GetByEntryCreatedAt(string entryCreatedAt);
+
+        Task<Extract> GetByEntryTransactionId(string transactionId);
+
+        Task<Extract> GetByEntryTransactionCounterPartyName(string counterPartyName);
+
+        Task<Extract> GetByEntryTransactionCounterPartyIdentity(string counterPartyIdentity);
+
+        Task<IEnumerable<Extract>> GetList();
+
+        Task Add(Extract extract);
+
+        void Update(Extract extract);
+
+        void Remove(Extract extract);
+    }
+}
