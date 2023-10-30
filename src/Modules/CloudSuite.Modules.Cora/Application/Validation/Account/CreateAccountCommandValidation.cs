bb05f715b1d151aecd2cd067e4645af338740290
development <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-10-30 17:44:11 -0400

    feat(#1219): Building CreateAccouyntCommandhandler

--- src/Modules/CloudSuite.Modules.Cora/Application/Validation/Account/CreateAccountCommandValidation.cs
+++ src/Modules/CloudSuite.Modules.Cora/Application/Validation/Account/CreateAccountCommandValidation.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CloudSuite.Modules.Cora.Application.Handlers.Account;
 
 namespace CloudSuite.Modules.Cora.Application.Validation.Account
 {
-	internal class CreateAccountCommandValidation
-	{
-	}
+    internal class CreateAccountCommandValidation
+    {
+        internal object Validate(CreateAccountCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
